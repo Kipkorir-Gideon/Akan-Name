774f82ab61f87e9d741c6d0f41f5279ee114dc08
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-05 11:39:02 +0300

    Add validation for gender selection.

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,15 @@ function akanNaming() {
             !bdayArray[2].match(/^\d\d$/)){
                 alert("Invalid date!");
         }
+        else if(gender.lenth===0){
+            alert("Please select your gender")
+        }
         else {
             document.getElementById("post").style.display= 'block';
             var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','friday','Saturday'];
             var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
             var femaleNames = ['Akosua','Adwoa','Abena','Akua','Yaa','Afua','Ama'];
-            var time = new Date(parseInt(dateOfBirth[0]),parseInt(dateOfBirth[1]) - 1,parseInt(dateOfBirth[2]));
+            var time = new Date(parseInt(dateOfBirth[0]), parseInt(dateOfBirth[1]) - 1, parseInt(dateOfBirth[2]));
             var day = time.getDay();
             var dayName = days[day];
 
