1d0143676c84504a1b9f6ddc272f0f68023ede25
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-05 13:08:59 +0300

    modify text for good readability.

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@ function akanNaming() {
         var birthday = document.getElementById("birthday").value;
         var gender = document.getElementById("gender").value;
         var dateOfBirth = birthday.split('-');
-        if(dateOfBirth.lenth !==3) {
+        if(dateOfBirth.length !==3) {
             alert("Invalid date!");
         }
-        else if(!bdayArray[0].match(/^\d\d\d\d$/) || 
-            !bdayArray[1].match(/^\d\d$/) || 
-            !bdayArray[2].match(/^\d\d$/)){
+        else if(!dateOfBirth[0].match(/^\d\d\d\d$/) || 
+            !dateOfBirth[1].match(/^\d\d$/) || 
+            !dateOfBirth[2].match(/^\d\d$/)){
                 alert("Invalid date!");
         }
-        else if(gender.lenth===0){
+        else if(gender.length===0){
             alert("Please select your gender")
         }
         else {
-            document.getElementById("post").style.display= 'block';
+            document.getElementById("output").style.display = 'block';
             var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','friday','Saturday'];
             var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
             var femaleNames = ['Akosua','Adwoa','Abena','Akua','Yaa','Afua','Ama'];
@@ -26,61 +26,61 @@ function akanNaming() {
             switch(gender) {
                 case "male":
                     if(dayName == days[0]){
-                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[0] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + maleNames[0] + ".";
                         return output;
                     }
                     else if(dayName == days[1]){
-                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[1] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + maleNames[1] + ".";
                         return output;
                     }
                     else if(dayName == days[2]){
-                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[2] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + maleNames[2] + ".";
                         return output;
                     }
                     else if(dayName == days[3]){
-                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[3] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + maleNames[3] + ".";
                         return output;
                     }
                     else if(dayName == days[4]){
-                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[4] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + maleNames[4] + ".";
                     }
                     else if(dayName == days[5]){
-                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[5] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + maleNames[5] + ".";
                         return output;
                     }
                     else if(dayName == days[6]){
-                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[6] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + maleNames[6] + ".";
                         return output;
                     }
                 break;
 
                 case "female":
                     if(dayName == days[0]){
-                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[0] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + femaleNames[0] + ".";
                         return output;
                     }
                     else if(dayName == days[1]){
-                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[1] + ".";
+                        var output = "You were born on " + dayName + "and your akan name is " + femaleNames[1] + ".";
                         return output;
                     }
                     else if(dayName == days[2]){
-                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[2] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + femaleNames[2] + ".";
                         return output;
                     }
                     else if(dayName == days[3]){
-                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[3] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + femaleNames[3] + ".";
                         return output;
                     }
                     else if(dayName == days[4]){
-                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[4] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + femaleNames[4] + ".";
                         return output;
                     }
                     else if(dayName == days[5]){
-                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[5] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + femaleNames[5] + ".";
                         return output;
                     }
                     else if(dayName == days[6]){
-                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[6] + ".";
+                        var output = "You were born on " + dayName + " and your akan name is " + femaleNames[6] + ".";
                         return output;
                     }
                 break;
