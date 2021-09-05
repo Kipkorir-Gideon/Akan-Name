73f0e35f31ef666e3d543e7bf99c9ceffed64290
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-05 11:25:36 +0300

    Add cases for gender.

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,69 @@ function akanNaming() {
             var time = new Date(parseInt(dateOfBirth[0]),parseInt(dateOfBirth[1]) - 1,parseInt(dateOfBirth[2]));
             var day = time.getDay();
             var dayName = days[day];
+
+            switch(gender) {
+                case "male":
+                    if(dayName == days[0]){
+                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[0] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[1]){
+                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[1] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[2]){
+                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[2] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[3]){
+                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[3] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[4]){
+                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[4] + ".";
+                    }
+                    else if(dayName == days[5]){
+                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[5] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[6]){
+                        var output = "You were born on" + dayName + "and your akan name is " + maleNames[6] + ".";
+                        return output;
+                    }
+                break;
+
+                case "female":
+                    if(dayName == days[0]){
+                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[0] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[1]){
+                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[1] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[2]){
+                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[2] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[3]){
+                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[3] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[4]){
+                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[4] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[5]){
+                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[5] + ".";
+                        return output;
+                    }
+                    else if(dayName == days[6]){
+                        var output = "You were born on" + dayName + "and your akan name is " + femaleNames[6] + ".";
+                        return output;
+                    }
+                break;
+            }
         }
     }
 }
