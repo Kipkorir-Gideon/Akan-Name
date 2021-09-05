01e5d60e39f57927ac72ed23c2e738efb5f46ad4
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-05 10:41:40 +0300

    Add form validation.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,18 @@ function akanNaming() {
     function akanName() {
         var birthday = document.getElementById("birthday").value;
         var gender = document.getElementById("gender").value;
-        
+        var dateOfBirth = birthday.split('-');
+        if(dateOfBirth.lenth !==3) {
+            alert("Invalid date!");
+        }
+        else if(!bdayArray[0].match(/^\d\d\d\d$/) || 
+            !bdayArray[1].match(/^\d\d$/) || 
+            !bdayArray[2].match(/^\d\d$/)){
+                alert("Invalid date!");
+        }
+        else {
+            document.getElementById("post").style.display= 'block';
+            
+        }
     }
 }
